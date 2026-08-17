05ab3b54c438419f331e7335fb0c627382b8d9fd
Vinícius Briches <vinisanto141@gmail.com>
2026-08-17 19:34:34 -0300

    Add files via upload

---
+++ equacao01.py
@@ -0,0 +1,14 @@
+# Descubra o número: “Um número somado ao seu dobro resulta em 36.” Qual é esse número?
+
+# Um número somado ao seu dobro resulta em 36
+# x + 2x = 36
+# 3x = 36
+# x = 36/3
+# x = 12
+
+x = 12
+resultado = x +(2 * x)
+print(f"O numero {x}, e seu dobro e {2*x}. Somados, o resultado e {resultado}.")
+
+# A equação é x + 2x = 36, então 3x = 36 e x = 12.
+# Nesse caso, o número é 12 e o seu dobro é 24, totalizando 36.
